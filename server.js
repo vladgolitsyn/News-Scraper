feb9c7cd5046be3cb6b79d7bdd4e25eb97043001
Vlad Golitsyn <vladgolitsyn@gmail.com>
2019-05-21 13:56:58 -0400

    added server.js file with required dependencies and configuration

---
+++ server.js
@@ -0,0 +1,36 @@
+// Required dependencies
+var express = require("express");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+
+// Set up our port to be either the host's designated port, or 3000
+var PORT = process.env.PORT || 3000;
+
+// Initiate express
+var app = express();
+
+// Reference required routes
+// var routes = require("./routes");
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect and initiate Handlebars html templeting
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Require the use of routes middleware
+// app.use(routes);
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
+// Listen on the port
+app.listen(PORT, function() {
+  console.log("Listening on http://localhost:" + PORT);
+});
